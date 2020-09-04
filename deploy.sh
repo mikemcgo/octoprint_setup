498fea673b22a872817cae830b03459594a6b3a8
mikemcgo <mike.mcgookey@gmail.com>
2020-09-04 15:39:40 -0400

    Config Files/systemd

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+cp interfaces /etc/network/interaces
+
+mkdir -p /etc/octoprint
+cp docker-compose@.service /etc/systemd/system/octoprint@.service
+cp -r mjpg /etc/octoprint
+cp -r octoprint /etc/octoprint
+
+systemctl start octoprint@myservice
